d3d4fe4ce5fdf75b853270c386ea78fb4296c9b7
answer-zf <feng18255163789@gmail.com>
2019-11-15 17:08:46 +0800

    edit:eslint.js

--- Config_Document/.eslintrc.js
+++ Config_Document/.eslintrc.js
@@ -1,24 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
-};
+  root: true,
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //强制使用单引号
+    quotes: ['error', 'single'],
+    //强制不使用分号结尾
+    semi: ['error', 'never'],
+    // 关闭冲突规则
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        parser: 'flow'
+      }
+    ]
+  }
+}
+// npm install --save-dev eslint-plugin-prettier eslint-config-prettier prettier-eslint-cli
