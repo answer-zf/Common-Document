0a48c4ee3bcaabeba282f55b48700b41edf82aaf
answer-zf <feng18255163789@gmail.com>
2020-05-20 17:53:00 +0800

    gogogo

--- __py_text/text.py
+++ __py_text/text.py
@@ -3,54 +3,65 @@
 """
 
 
-class Employee:
+class StudentModel:
     """
-        普通员工类 即 父类
+        学生数据模型类
     """
 
-    def __init__(self, name, job):
+    def __init__(self, name="", age=0, score=0, id=0):
+        """
+            创建学生对象
+        :param id: 编号
+        :param name: 姓名
+        :param age: 年龄
+        :param score: 成绩
+        """
+        self.id = id
         self.name = name
-        self.job = job  # 依赖
+        self.age = age
+        self.score = score
 
-    def calcular_salary(self):
-        return self.job.get_salary()
+    def __str__(self):
+        return "id is %d, name is %s, age is %d, score is %d" % (self.id, self.name, self.age, self.score)
+
+    def __repr__(self):
+        return 'StudentModel("%s", %d, %d)' % (self.name, self.age, self.score)
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        self.__age = value
+
+    @property
+    def score(self):
+        return self.__score
+
+    @score.setter
+    def score(self, value):
+        self.__score = value
 
 
-class Job:
-    def __init__(self, basic_salary):
-        self.basic_salary = basic_salary
-
-    def get_salary(self):
-        return self.basic_salary
-
-
-class Programer(Job):  # 关联
-    """
-        程序员类
-    """
-
-    def __init__(self, basic_salary, extra):
-        super().__init__(basic_salary)
-        self.extra = extra
-
-    def get_salary(self):
-        return super().get_salary() + self.extra
-
-
-class salesmen(Job):
-    """
-        销售类
-    """
-
-    def __init__(self, basic_salary, extra):
-        super().__init__(basic_salary)
-        self.extra = extra
-
-    def get_salary(self):
-        return super().get_salary() + self.extra
-
-
-lw = Employee("lw", Programer(100, 10))
-print(lw.calcular_salary())
-lw.job = salesmen(100, 1)
-print(lw.calcular_salary())
+s01 = StudentModel('zf', 12, 90)
+print(s01)
+re = eval(s01.__repr__())
+print(re)
+list_01 = ["zf", 44, 99]
