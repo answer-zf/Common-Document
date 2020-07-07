bbef0e856ea96c53b394235119be72406fbce2f5
answer-zf <feng18255163789@gmail.com>
2020-07-07 17:54:03 +0800

    home

--- __py_text/PythonNet/tcp_client.py
+++ __py_text/PythonNet/tcp_client.py
@@ -5,3 +5,15 @@
 from socket import *
 
 sockfd = socket()
+
+server_addr = ("127.0.0.1", 12016)
+
+sockfd.connect(server_addr)
+
+sockfd.send(b"answer-zf")
+
+data = sockfd.recv(1024)
+
+print(data.decode())
+
+sockfd.close()
--- __py_text/PythonNet/text.py
+++ __py_text/PythonNet/text.py
@@ -1,5 +1,17 @@
 """
-    复制文件并重命名
+
 """
+
+# import time
+#
+# while True:
+#     file_obj = open("text.txt", "a+")
+#     now_offset = file_obj.tell()
+#     file_obj.seek(0)
+#     file_row = len(file_obj.readlines())
+#     file_obj.seek(now_offset)
+#     file_content = "%d. %s\n" % (file_row, time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
+#     file_obj.write(file_content)
+#     time.sleep(1)
 
 
